b9f8e50883fac32be3872171f7c75a48411d67c5
Eduardo Varela Sousa <eduardo.varela@outlook.com.br>
2023-02-12 19:18:48 -0300

    Refatorando a Home e o Header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,88 @@
+import {
+  Card, Container, InputSearchContainer, Header, ListContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export default function Home() {
-  return 'Homepage';
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Eduardo Varela</strong>
+              <small>instagram</small>
+            </div>
+            <span>eduardo@dev.com.br</span>
+            <span>(11) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Eduardo Varela</strong>
+              <small>instagram</small>
+            </div>
+            <span>eduardo@dev.com.br</span>
+            <span>(11) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Eduardo Varela</strong>
+              <small>instagram</small>
+            </div>
+            <span>eduardo@dev.com.br</span>
+            <span>(11) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }

b61f4ffe637e981c807f3e578f4c692648a7fd61
Eduardo Varela Sousa <eduardo.varela@outlook.com.br>
2023-02-12 19:51:00 -0300

    Criando os components de input, select e button

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -3,7 +3,7 @@ export default {
     background: '#F6F5FC',
     primary: {
       lighter: '#E0E3FF',
-      ligh: '#6674F4',
+      light: '#6674F4',
       main: '#5061FC',
       dark: '#3346F0',
     },
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,27 @@
 import PageHeader from '../../components/PageHeader';
 
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Button from '../../components/Button';
+
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo contato"
-    />
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Facebook</option>
+        <option value="123">Discord</option>
+        <option value="123">Linkedin</option>
+        <option value="123">E-mail</option>
+      </Select>
+      <Button type="button">
+        Salvar alterações
+      </Button>
+    </>
   );
 }

65bf90ae2937f288cefb1ee5fa50064a05bbf6c3
Eduardo Varela Sousa <eduardo.varela@outlook.com.br>
2023-02-13 01:03:47 -0300

    Criando modal de confirmação e loader

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -53,7 +53,7 @@ export const ListContainer = styled.div`
 
 export const Card = styled.div`
   background: #fff;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.4);
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
   padding: 16px;
   border-radius: 4px;
   display: flex;
@@ -108,7 +108,7 @@ export const InputSearchContainer = styled.div`
     border: none;
     border-radius: 25px;
     height: 50px;
-    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.4);
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
     outline: 0;
     padding: 0 16px;
 
